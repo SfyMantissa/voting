d3b0ae089dcc993b7998dfec9a92baffdb5146f5
Sfy Mantissa <sfymantissa@protonmail.com>
2022-04-29 23:41:36 +0300

    Added deploy script.

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+const main = async () => {
+  const Voting = await hre.ethers.getContractFactory("Voting");
+  const voting = await Voting.deploy();
+  await voting.deployed();
+  console.log("Voting deployed to:", voting.address);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
